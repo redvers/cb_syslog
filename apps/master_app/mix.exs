8d93ea8ecd2add718d5f3b285a0c702650babc19
red <red_davies@belk.com>
2016-02-10 08:36:42 -0500

    Version bump + release

--- apps/master_app/mix.exs
+++ apps/master_app/mix.exs
@@ -3,7 +3,7 @@ defmodule MasterApp.Mixfile do
 
   def project do
     [app: :master_app,
-     version: "0.0.1",
+     version: "0.0.2",
      build_path: "../../_build",
      config_path: "../../config/config.exs",
      deps_path: "../../deps",
