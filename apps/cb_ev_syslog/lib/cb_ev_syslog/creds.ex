ede578dfbc5587153684ab1ad4b121dc5c892676
Red Davies <red@infect.me>
2016-02-17 16:06:22 -0500

    Added temporary LDAP lookup functions

--- apps/cb_ev_syslog/lib/cb_ev_syslog/creds.ex
+++ apps/cb_ev_syslog/lib/cb_ev_syslog/creds.ex
@@ -18,5 +18,27 @@ defmodule CbEvSyslog.Creds do
     |> Enum.reduce(%{}, fn(x, acc) -> [field, value] = String.split(x, "=") ; Map.put(acc, String.downcase(field), value) end)
   end
 
+  def ldapcreds do
+    ldapcreddata = File.read!("/etc/cb/cb.conf")
+    |> String.split("\n")
+    |> Enum.filter(&(Regex.match?(~r/^#ldapad/i, &1)))
+#    |> Enum.map(%{"#ldapaddomain" => %{}, "#ldapaduser" => "", "#ldapadpass" => ""}, fn(x,acc) ->
+    |> Enum.map(&parse_ldap_line/1)
+  end
+
+  def parse_ldap_line(x) do
+    [key, val] = String.split(x, ":")
+    case key do
+      "#ldapaddomain" ->
+        [domain, dn] = String.split(val, ";")
+        {domain, dn}
+      other ->
+        {key, val}
+    end
+  end
+
+
+
+
 
 end
